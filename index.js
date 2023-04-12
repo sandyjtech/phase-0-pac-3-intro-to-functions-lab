d373609508c065751c7ed935ad8ff09e5b61fed5
Sandra <sandyjtech@gmail.com>
2023-04-12 12:36:08 -0700

    added index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function shout(myString) {
+    return myString.toUpperCase();
+
+}
+
+function whisper(myString) {
+    return myString.toLowerCase();
+
+}
+
+function logShout(myString) {
+    console.log(myString.toUpperCase());
+
+}
+function logWhisper(myString) {
+    console.log(myString.toLowerCase());
+
+}
+
+function sayHiToHeadphonedRoommate(myString) {
+
+    if (myString.toLowerCase(myString) === myString) {
+   return "I can't hear you!";
+
+} else if (myString.toUpperCase(myString) === myString ) {
+   return "YES INDEED!";
+
+} else {
+    return "I would love to!";
+}
+
+}
